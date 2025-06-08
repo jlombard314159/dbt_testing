819f9b8fb394a0d770dd69b985acb915133f933e
John Lombardi <jlombard314@gmail.com>
2025-06-08 12:11:06 -0600

    additional notes

--- dbt_runner.sh
+++ dbt_runner.sh
@@ -5,3 +5,14 @@ cd ./dbt_testing
 dbt debug
 dbt run 
 dbt test
+
+# Some additional things
+
+#Generate SQL but no execution
+dbt compile
+
+#Create documentation
+dbt docs generate
+
+# Access the models. NOTE: Must run docs generate
+dbt docs serve
